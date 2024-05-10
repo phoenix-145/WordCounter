4b69b3119b8711862b559b4984137408310903ab
Aung Kyaw Myo <harish2002710@gmail.com>
2024-05-10 17:11:27 +0800

    Cleaned up code and defined clear variables

--- OpensubtitlesAPI/OpensubtitlesAPI.cs
+++ OpensubtitlesAPI/OpensubtitlesAPI.cs
@@ -7,12 +7,14 @@ namespace WordCounter
     {
         private static readonly HttpClient httpClient= new HttpClient();
         private static readonly string OpensubtitlesAPI_Key = ConfigurationManager.AppSettings.Get("OpensubtitlesAPI_Key")!;
-        // private static string _username;
-        // private static string _password;
+        private static readonly string OpensubtitlesAPI_Username = ConfigurationManager.AppSettings.Get("OpensubtitlesAPI_Username")!;
+        private static readonly string OpensubtitlesAPI_Password = ConfigurationManager.AppSettings.Get("OpensubtitlesAPI_Password")!;
+        private static readonly string OpensubtitlesLoginURL = "https://api.opensubtitles.com/api/v1/login";
         public static async Task MakeConnection()
         {
             await Login();
             Console.ReadLine();
+
             var client = httpClient;
             var request = new HttpRequestMessage
             {
@@ -52,14 +54,14 @@ namespace WordCounter
             var request = new HttpRequestMessage
             {
                 Method= HttpMethod.Post,
-                RequestUri = new Uri("https://api.opensubtitles.com/api/v1/login"),
+                RequestUri = new Uri(OpensubtitlesLoginURL),
                 Headers = 
                 {
                     { "User-Agent", "WordCounter/1.0" },
                     { "Accept", "application/json" },
                     { "Api-Key", OpensubtitlesAPI_Key }
                 },
-                Content = new StringContent("{\n  \"username\": \"phoenix145\",\n  \"password\": \"Aungkyawmyo_13\"\n}")
+                Content = new StringContent($"{{\n  \"username\": \"{OpensubtitlesAPI_Username}\",\n  \"password\": \"{OpensubtitlesAPI_Password}\"\n}}")
                 {
                     Headers =
                     {
